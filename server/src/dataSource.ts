dac3c58347bcd3ebb6955b8a65b143aea5b65650
TijanaSSS <tijanaslavkovic65@gmail.com>
2022-06-03 22:29:18 +0200

    Dodate domenske klase i migracije

---
+++ server/src/dataSource.ts
@@ -0,0 +1,12 @@
+import { DataSource } from "typeorm";
+
+export const appDataSource = new DataSource({
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "root",
+  password: "",
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  database: "mreza",
+})
